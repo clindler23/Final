c1f4f1ac54f5afd2074a62a39e4bc377908c0abf
clindler23 <christian.Lindler@Outlook.com>
2023-01-15 22:59:43 -0500

    added

---
+++ wwwroot/classes/autoload.php
@@ -0,0 +1,13 @@
+<?php 
+	
+	session_start();
+
+	include("classes2/connect.php");
+	include("classes2/functions.php");
+	include("classes2/login.php");
+	include("classes2/user.php");
+	include("classes2/post.php");
+ 	include("classes2/image.php");
+ 	include("classes2/profile.php");
+ 	include("classes2/settings.php");
+ 	include("classes2/time.php");
